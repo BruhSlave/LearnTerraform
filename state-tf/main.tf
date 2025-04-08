db4931e5b122a7cb698fa1aab9927f36629dbe74
BruhSlave <braslpaf@gmail.com>
2025-04-08 19:53:23 +0000

    Add state-tf/import.tf

--- state-tf/main.tf
+++ state-tf/main.tf
@@ -1,5 +1,5 @@
 locals {
-    zone = "ru-central1-b"
+    zone = "ru-central1-a"
 }
 
 resource "yandex_vpc_network" "dev" {
@@ -9,7 +9,7 @@ resource "yandex_vpc_network" "dev" {
 resource "yandex_vpc_subnet" "dev" {
   name = "test-subnet"
   zone = local.zone
-  network_id = yandex_vpc_network.dev
+  network_id = yandex_vpc_network.dev.id
   v4_cidr_blocks = ["10.5.0.0/24"]
 }
 
@@ -17,10 +17,10 @@ data "yandex_compute_image" "dev" {
     family = "container-optimized-image"
 }
 
-resource "yandex_compute_disk" "dev" {
-  name = "test-vm-disk"
+resource "yandex_compute_disk" "testvm" {
+  name = "testvm-disk"
   zone = local.zone
-  image_id = data.yandex_compute_image.image_id
+  image_id = data.yandex_compute_image.dev.image_id
   size = "15"
 
   lifecycle {
